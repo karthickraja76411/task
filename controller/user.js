dc1181ef08d6ecdf24171cf186d5476f844b32ea
karthickraja76411 <davidjkej@gmail.com>
2024-03-30 13:34:55 +0530

    changes

--- controller/user.js
+++ controller/user.js
@@ -6,6 +6,8 @@ import { uploads } from '../middlewares/uploadFile.js';
 import fs from "fs";
 import { fileURLToPath } from 'url';
 import spath from "path";
+import nodemailer from "nodemailer";
+import emailExistence from "email-existence";
 
 
 export const userRouter = Router();
@@ -25,27 +27,69 @@ userRouter.post("/add", uploads.single("profileImg"), async (req, res) => {
         if (typeof name !== "undefined" && name !== "" && typeof email !== "undefined" && email !== "" && typeof password !== "undefined" && password !== "") {
             const findEmail = await UserModal.find({ $or: [{ email }, { phoneNumber }] })
             if (findEmail.length === 0) {
-                if (req.file) {
-                    const { path, originalname } = req.file;
-                    const extName = originalname.split(".")[1];
-                    const randomNum = Date.now();
-                    const newFileName = `${name}_${randomNum}.${extName}`;
-                    var convertedFilePath = `/upload/userProfiles/${name}/${newFileName}`;
-                    // Rename the File Name
-                    const __filename = fileURLToPath(import.meta.url);
-                    const dir = spath.join(spath.dirname(__filename), '..',);
-                    fs.rename(path, dir + convertedFilePath, (err) => {
-                        if (err) { console.log("error in File Rename", err.message); }
-                    })
-                }
-                var hashPaswword = bcrypt.hashSync(password, 10);
-                const insertQuery = new UserModal({ name, email, password: hashPaswword, phoneNumber, profileImg: convertedFilePath });
-                await insertQuery.save();
-                return res.status(200).json({ status: 1, message: "User created successfully" })
-            } else {
-                fs.unlink(req.file.path, (err) => {
-                    if (err) { console.log("error in unlink", err.message); }
+
+                const transpoter = nodemailer.createTransport({
+                    service: "gmail",
+                    auth: {
+                        user: "pistolpd19@gmail.com",
+                        pass: "nzmd dcvy dgac puzu"
+                    },
+                    tls: {
+                        rejectUnauthorized: false
+                    },
+
                 })
+
+                const mailOptions = {
+                    from: 'pistolpd19@gmail.com',
+                    to: email,
+                    subject: 'Verification',
+                    html: `
+                      <p>Hello ${name},</p>
+                      <p>It's simple verification for our side</p>
+                    `,
+                    dsn: {
+                        id: Math.random() * 10000000000,
+                        return: 'headers',
+                        notify: ['failure', 'delay'],
+                        recipient: 'sender@example.com'
+                    }
+                };
+                emailExistence.check(email, function (error, response) {
+                    if (response) {
+                        transpoter.sendMail(mailOptions, async (err, info) => {
+                            if (err) throw err;
+                            else {
+                                if (req.file) {
+                                    const { path, originalname } = req.file;
+                                    const extName = originalname.split(".")[1];
+                                    const randomNum = Date.now();
+                                    const newFileName = `${name}_${randomNum}.${extName}`;
+                                    var convertedFilePath = `/upload/userProfiles/${name}/${newFileName}`;
+                                    // Rename the File Name
+                                    const __filename = fileURLToPath(import.meta.url);
+                                    const dir = spath.join(spath.dirname(__filename), '..',);
+                                    fs.rename(path, dir + convertedFilePath, (err) => {
+                                        if (err) { console.log("error in File Rename", err.message); }
+                                    })
+                                }
+                                var hashPaswword = bcrypt.hashSync(password, 10);
+                                const insertQuery = new UserModal({ name, email, password: hashPaswword, phoneNumber, profileImg: convertedFilePath });
+                                await insertQuery.save();
+                                return res.status(200).json({ status: 1, message: "User created successfully" })
+                            }
+                        })
+                    } else {
+                        fs.unlink(req.file.path, (err) => {
+                            if (err) { console.log("error in unlink", err.message); }
+                        })
+                        return res.status(400).json({ status: 0, message: "It's not valid email" });
+                    }
+                });
+
+
+
+            } else {
                 return res.status(400).json({ status: 0, message: "User already exists" });
             }
         } else {
